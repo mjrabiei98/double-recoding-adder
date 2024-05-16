2768b198fc1ea064867147081c135f8d060e4847
mohammadjavad rabiei <mj.rabiei98@gmail.com>
2024-05-16 17:11:06 +0330

    signed digit adder with double recoding added

--- double_recoding_adder.vhdl
+++ double_recoding_adder.vhdl
@@ -6,7 +6,7 @@ ENTITY hz IS
     -- z {-2,-1,0} h {0,1}
     PORT (
         xi, yi : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-        h : OUT STD_LOGIC;
+        h : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
         z : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)
     );
 END ENTITY hz;
@@ -18,19 +18,19 @@ BEGIN
     BEGIN
         temp_sum := STD_LOGIC_VECTOR(signed(xi) + signed(yi));
         IF temp_sum = "00" THEN
-            h <= '0';
+            h <= "00";
             z <= "00";
         ELSIF temp_sum = "01" THEN
-            h <= '1';
+            h <= "01";
             z <= "11";
         ELSIF temp_sum = "10" THEN
-            h <= '0';
+            h <= "00";
             z <= "10";
         ELSIF temp_sum = "11" THEN
-            h <= '0';
+            h <= "00";
             z <= "10";
         ELSE
-            h <= '1';
+            h <= "01";
             z <= "00";
         END IF;
 
@@ -46,14 +46,14 @@ USE IEEE.numeric_std.ALL;
 ENTITY adder IS
     -- q {-2,-1,0,1}
     PORT (
-        h : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-        z : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+        a : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+        b : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
         q : OUT STD_LOGIC_VECTOR(1 DOWNTO 0));
 END ENTITY adder;
 
 ARCHITECTURE behavioral OF adder IS
 BEGIN
-    q <= STD_LOGIC_VECTOR(signed(z) + signed(h));
+    q <= STD_LOGIC_VECTOR(signed(a) + signed(b));
 END behavioral; -- behavioral
 
 ------------------------
@@ -96,5 +96,44 @@ BEGIN
     END PROCESS;
 
 END behavioral; -- behavioral
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
+PACKAGE mypackage IS
+    TYPE sd_array IS ARRAY (NATURAL RANGE <>) OF STD_LOGIC_VECTOR(1 DOWNTO 0);
+    TYPE d_array IS ARRAY (NATURAL RANGE <>) OF STD_LOGIC;
+END;
 
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
+USE work.mypackage.ALL;
+
+ENTITY double_recoding_adder IS
+    GENERIC (input_size : INTEGER := 16);
+    PORT (
+        x, y : IN sd_array (input_size - 1 DOWNTO 0);
+        sum : OUT sd_array (input_size - 1 DOWNTO 0)
+    );
+END ENTITY double_recoding_adder;
+ARCHITECTURE behavioral OF double_recoding_adder IS
+
+    SIGNAL h : sd_array (input_size DOWNTO 0);
+    SIGNAL z : sd_array (input_size - 1 DOWNTO 0);
+    SIGNAL t : sd_array (input_size DOWNTO 0);
+    SIGNAL w : sd_array (input_size - 1 DOWNTO 0);
+    SIGNAL q : sd_array (input_size - 1 DOWNTO 0);
+
+BEGIN
+    h(0) <= "00";
+    t(0) <= "00";
+    u : FOR i IN 0 TO input_size - 1 GENERATE
+    BEGIN
+        hzi : ENTITY work.hz(behavioral) PORT MAP(xi => x(i), yi => y(i), H => h(i + 1), Z => z(i));
+        adder1_i : ENTITY work.Adder(behavioral) PORT MAP(a => z(i), b => h(i), q => q(i));
+        twi : ENTITY work.TW(behavioral) PORT MAP(Q => q(i), w => w(i), t => t(i + 1));
+        adder2_i : ENTITY work.Adder(behavioral) PORT MAP(a => t(i), b => w(i), q => sum(i));
+    END GENERATE;
+
+END behavioral;
---
+++ testbench.vhdl
@@ -0,0 +1,35 @@
+-- LIBRARY ieee;
+-- USE ieee.std_logic_1164.ALL;
+-- USE ieee.numeric_std.ALL;
+
+-- USE work.mypackage.ALL;
+
+-- ENTITY tester IS END ENTITY;
+-- ARCHITECTURE TEStER_ARC OF tester IS
+--     SIGNAL xx1 : array_UI (3 DOWNTO 0);
+--     SIGNAL yy1 : array_UI (3 DOWNTO 0);
+--     SIGNAL ss1 : array_UI (3 DOWNTO 0);
+--     SIGNAL cc1 : STD_LOGIC_VECTOR(1 DOWNTO 0);
+-- BEGIN
+--     UUT : ENTITY work.Signed_digit GENERIC MAP(4) PORT MAP(xx1, yy1, ss1, cc1);
+--     PROCESS BEGIN
+--         xx1(0) <= "00";
+--         xx1(1) <= "01";
+--         xx1(2) <= "01";
+--         xx1(3) <= "00";
+--         yy1(0) <= "00";
+--         yy1(1) <= "11";
+--         yy1(2) <= "01";
+--         yy1(3) <= "01";
+--         WAIT FOR 50 NS;
+--         xx1(0) <= "00";
+--         xx1(1) <= "01";
+--         xx1(2) <= "00";
+--         xx1(3) <= "11";
+--         yy1(0) <= "00";
+--         yy1(1) <= "01";
+--         yy1(2) <= "00";
+--         yy1(3) <= "01";
+--         WAIT;
+--     END PROCESS;
+-- END ARCHITECTURE TEStER_ARC;
