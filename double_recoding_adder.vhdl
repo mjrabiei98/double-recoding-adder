8e46adc81f8f49044f0a917b0b59000912fdd7ef
mohammadjavad rabiei <mj.rabiei98@gmail.com>
2024-05-16 15:57:20 +0330

    hz component added

--- double_recoding_adder.vhdl
+++ double_recoding_adder.vhdl
@@ -1,4 +1,4 @@
-LIBRARY library IEEE;
+LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
 
@@ -6,44 +6,64 @@ ENTITY hz IS
     -- z {-2,-1,0} h {0,1}
     PORT (
         xi, yi : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-        h : OUT STD_LOGIC, z : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)
+        h : OUT STD_LOGIC;
+        z : OUT STD_LOGIC_VECTOR(1 DOWNTO 0)
     );
 END ENTITY hz;
 
 ARCHITECTURE behavioral OF hz IS
-
-    SIGNAL temp_sum : std_logic_vector(7 downto 0)
-
 BEGIN
+    PROCESS (xi, yi)
+        VARIABLE temp_sum : STD_LOGIC_VECTOR(1 DOWNTO 0);
+    BEGIN
+        temp_sum := STD_LOGIC_VECTOR(signed(xi) + signed(yi));
+        IF temp_sum = "00" THEN
+            h <= '0';
+            z <= "00";
+        ELSIF temp_sum = "01" THEN
+            h <= '1';
+            z <= "11";
+        ELSIF temp_sum = "10" THEN
+            h <= '0';
+            z <= "10";
+        ELSIF temp_sum = "11" THEN
+            h <= '0';
+            z <= "10";
+        else
+            h <= '1';
+            z <= "00";
+        END IF;
 
+    END PROCESS;
 END behavioral; -- behavioral
-ENTITY adder IS
-    -- q {-2,-1,0,1}
-    PORT (
-        h : IN STD_LOGIC;
-        z : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-        q : OUT STD_LOGIC_VECTOR(1 DOWNTO 0));
-END ENTITY adder;
 
-ARCHITECTURE behavioral OF adder IS
+-- ENTITY adder IS
+--     -- q {-2,-1,0,1}
+--     PORT (
+--         h : IN STD_LOGIC;
+--         z : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+--         q : OUT STD_LOGIC_VECTOR(1 DOWNTO 0));
+-- END ENTITY adder;
 
-    SIGNAL
+-- ARCHITECTURE behavioral OF adder IS
 
-BEGIN
+--     SIGNAL
 
-END behavioral; -- behavioral
-ENTITY tw IS
-    -- q {-2,-1,0,1}
-    PORT (
-        h : IN STD_LOGIC;
-        z : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-        q : OUT STD_LOGIC_VECTOR(1 DOWNTO 0));
-END ENTITY tw;
+-- BEGIN
 
-ARCHITECTURE behavioral OF tw IS
+-- END behavioral; -- behavioral
+-- ENTITY tw IS
+--     -- q {-2,-1,0,1}
+--     PORT (
+--         h : IN STD_LOGIC;
+--         z : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+--         q : OUT STD_LOGIC_VECTOR(1 DOWNTO 0));
+-- END ENTITY tw;
 
-    SIGNAL
+-- ARCHITECTURE behavioral OF tw IS
 
-BEGIN
+--     SIGNAL
 
-END behavioral; -- behavioral
+-- BEGIN
+
+-- END behavioral; -- behavioral
